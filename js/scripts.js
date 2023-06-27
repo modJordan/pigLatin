ba5bb057db198e39262d75879565098b4b5642af
modjordan <mod.jordan@icloud.com>
2023-06-26 20:45:46 -0700

    WIP 2nd test to add 'way' to end of vowel words.

--- js/scripts.js
+++ js/scripts.js
@@ -10,7 +10,39 @@ function isEmpty() {
 }
 
 // Business Logic
+// 1. split up string into array
+// 2. want find indexes in array that start with a vowels
+// 3. we want to add "way" to the end 
+// 4. we want to rejoin the string together 
+// 5. we want to output the result of the function
 
+function vowels2(string) {
+  if (isEmpty(string)) {
+    return string;
+  }
+  let stringArr = string.split(" ");
+  for (i = 0; i < stringArr.length; i++) {
+    if (stringArr[i].includes(/[^aeiou]/)) {
+      let strWay = strWay.concat("way");
+      let newStr = strWay.join(" ");
+      return newStr;
+    }
+  }
+}
+
+function vowels(string) {
+  if (isEmpty(string)) {
+    return string;
+  } for (i = 0; i < string.length; i++) {
+    if (string.charAt(0)[aeiou]) {
+      return string.concat("way");
+    }
+    let strArray = string.split(" ")
+    const vowelWords = strArray.map(function () {
+      return vowelWords.join(" ");
+    });
+  }
+}
 
 
 // UI Logic
@@ -18,9 +50,28 @@ function isEmpty() {
 function handleFormSubmission(e) {
   e.preventDefault();
   let passage = document.getElementById("form-latin").value;
-  let latinArr = [passage]
+  let latinArr = passage.split(" ");
+
 }
 
 window.addEventListener("load", function () {
   document.querySelector("form#form-latin").addEventListener("submit", handleFormSubmission);
 });
+
+
+
+function vowels(string) {
+  if (isEmpty(string)) {
+    return string;
+  }
+  for (i = 0; i < string.length; i++) {
+    if (string.charAt(i)(/[aeiou]/i)) {
+      return string.concat("way");
+    }
+  }
+  let strArray = string.split(" ")
+  const vowelWords = strArray.map(function (vowels) {
+    return vowelWords.join(" ");
+  });
+}
+}
